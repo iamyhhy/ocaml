f4d573f891322d5942112e0ea5baa9f65c82a1e4
iamyhhy <yhhy@umich.edu>
2017-10-25 16:06:51 -0400

    add fun

---
+++ print_hello.ml
@@ -0,0 +1,62 @@
+(* Starting practice: generate a print_hello function*)
+open Cil
+
+
+
+
+let main () = begin
+    let files = ref [] in
+
+    let usage_message = "Just add a print_hello function in a C code" in
+    let args = [] in
+    Arg.parse (Arg.align args)
+        (*"::" is "append" to a list*)
+        (fun filename -> files := filename :: !files) usage_message;
+
+    Cil.initCIL ();
+    
+    List.iter (fun filename -> 
+        Printf.printf "%s: parsing\n" filename;
+        let ast = Frontc.parse filename () in
+        Printf.printf "%s: parsed\n" filename;
+
+        (*construct the print_hello function and add it to AST*)
+        (*fundec(svar, sformals, slocals, smaxid, sbody, smaxstmtid,
+         * sallstmts)*)
+        let new_exp = Const(CStr("Hello World\n")) in 
+        (* find the printf in ast and get the varinfo of it and replace the
+         * printf_varinfo here: findOrCreateFunc*)
+        let printf_varinfo = findOrCreateFunc ast "printf"
+        (TFun( TVoid([]),
+              Some([
+                  ("to_print", 
+                   TPtr(
+                       Cil.charType,
+                       []),
+                       []
+                   )
+                   ]),
+              false,
+              [] )) in
+        
+        let printf_instr = Call(None, Lval(Var printf_varinfo, NoOffset),
+        [new_exp],locUnknown) in
+        let new_fundec = emptyFunction "print_hello" in
+        setFunctionTypeMakeFormals new_fundec (TFun(TVoid([]), Some([]), false,[]));
+        Cil.setMaxId new_fundec;
+        new_fundec.sbody <- ( mkBlock [(mkStmt (Instr( [printf_instr] )))] );
+        computeCFGInfo new_fundec true; 
+       
+        let hello_func = GFun(new_fundec, locUnknown) in
+       
+        ast.globals <-  (GVarDecl(new_fundec.svar,locUnknown) :: ast.globals) @ [hello_func];
+
+
+  
+
+        dumpFile defaultCilPrinter stdout filename ast ;
+
+    ) !files ;
+
+end;;
+main ();;
